5150c116b831acb9265a5ba238ee35a197883336
Anton de Winter <adewinter@gmail.com>
2022-01-31 12:31:02 -0800

    Headtracking implementation. Stripping out scatter-gl because it's google abandonware causing way more problems than it's worth.

---
+++ headtracking/headtracking.js
@@ -0,0 +1,202 @@
+import {Camera} from './camera.js';
+import {STATE, setBackendAndEnvFlags} from './params.js';
+import {setupStats} from "./stats_panel.js"
+
+let detector, camera, stats;
+let startInferenceTime, numInferences = 0;
+let inferenceTimeSum = 0, lastPanelUpdate = 0;
+let rafId;
+
+const LEFT_EYE_INDEX = 2;
+const RIGHT_EYE_INDEX = 5;
+
+let posXEl, posYEl, posZEl, calcZEl;
+let posX3dEl, posY3dEl, posZ3dEl, calcZ3dEl;
+
+// const SCREEN_HEIGHT_METERS = 0.2794;
+// const SCREEN_WIDTH_METERS = 9999
+
+
+// const blazepose_distance_ratio_map = {
+//   // [z, actual_head_distance_from_screen_in_meters]
+//   [-1.00, 0.60],
+//   [-2.00, 0.30]
+// }
+
+async function createDetector() {
+
+    // let detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
+    // detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+    let detectorConfig = {
+      runtime: 'mediapipe',
+      enableSmoothing: true,
+      modelType: 'full',
+      solutionPath: 'node_modules/@mediapipe/pose',
+      // solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/pose',
+      // detectorModelUrl: '/headtracking/blazepose-detector-model.json',
+      // detectorModelUrl: '/headtracking/model.json',
+      // landmarkModelUrl: '/headtracking/blazepose-landmark-model.json'
+    };
+    detector = await poseDetection.createDetector(poseDetection.SupportedModels.BlazePose, detectorConfig);
+    return detector;
+}
+
+function beginEstimatePosesStats() {
+  startInferenceTime = (performance || Date).now();
+}
+
+function endEstimatePosesStats() {
+  const endInferenceTime = (performance || Date).now();
+  inferenceTimeSum += endInferenceTime - startInferenceTime;
+  ++numInferences;
+
+  const panelUpdateMilliseconds = 1000;
+  if (endInferenceTime - lastPanelUpdate >= panelUpdateMilliseconds) {
+    const averageInferenceTime = inferenceTimeSum / numInferences;
+    inferenceTimeSum = 0;
+    numInferences = 0;
+    stats.customFpsPanel.update(
+        1000.0 / averageInferenceTime, 120 /* maxValue */);
+    lastPanelUpdate = endInferenceTime;
+  }
+}
+
+function getCoordsFromKeypoint(kp) {
+  // For webcam z=-1, the distance of head from camera is ~60cm
+  // when z=-2, distance from webcam is ~30cm. (when centered)
+  // x and y values given by Keypoint seem to be regular pixel values of where the landmark occurs in the image
+  let kp_left = kp[LEFT_EYE_INDEX];
+  let kp_right = kp[RIGHT_EYE_INDEX];
+
+  let avgz = (kp_left['z'] + kp_right['z'])/2;
+  let avgx = (kp_left['x'] + kp_right['x'])/2;
+  let avgy = (kp_left['y'] + kp_right['y'])/2;
+  let calcz = 0.6/avgz;
+
+
+
+  // console.log("Distance: ", (0.6/avgz)*-1, "x:", avgx, "y:", avgy, "z: ", avgz);
+  return [avgx, avgy, avgz, calcz];
+}
+
+function rendercoords (kp, kp3d) {
+    let [avgx, avgy, avgz, calcz] = getCoordsFromKeypoint(kp);
+    posXEl.innerText = avgx.toFixed(4);
+    posYEl.innerText = avgy.toFixed(4);
+    posZEl.innerText = avgz.toFixed(4);
+    calcZEl.innerText = (0.6/avgz).toFixed(4);
+
+    [avgx, avgy, avgz, calcz] = getCoordsFromKeypoint(kp3d);
+    posX3dEl.innerText = avgx.toFixed(4);
+    posY3dEl.innerText = avgy.toFixed(4);
+    posZ3dEl.innerText = avgz.toFixed(4);
+    calcZ3dEl.innerText = (0.6/avgz).toFixed(4);
+}
+
+let counter = 0;
+async function renderResult() {
+  if (camera.video.readyState < 2) {
+    await new Promise((resolve) => {
+      camera.video.onloadeddata = () => {
+        resolve(video);
+      };
+    });
+  }
+
+  let poses = null;
+
+  // Detector can be null if initialization failed (for example when loading
+  // from a URL that does not exist).
+  if (detector != null) {
+    // FPS only counts the time it takes to finish estimatePoses.
+    beginEstimatePosesStats();
+
+    // Detectors can throw errors, for example when using custom URLs that
+    // contain a model that doesn't provide the expected output.
+    try {
+      poses = await detector.estimatePoses(
+          camera.video,
+          {maxPoses: STATE.modelConfig.maxPoses, flipHorizontal: false});
+    } catch (error) {
+      detector.dispose();
+      detector = null;
+      alert(error);
+    }
+
+    endEstimatePosesStats();
+  }
+
+  camera.drawCtx();
+
+  // The null check makes sure the UI is not in the middle of changing to a
+  // different model. If during model change, the result is from an old model,
+  // which shouldn't be rendered.
+  if (poses && poses.length > 0 && !STATE.isModelChanged) {
+    let hasKeypoints = poses[0] && poses[0].keypoints && poses[0].keypoints.length > 3;
+    let kp = poses[0].keypoints;
+    let kp3d = poses[0].keypoints3D;
+    if(hasKeypoints) {
+      counter += 1;
+      if(counter % 1 == 0) {
+        rendercoords(kp, kp3d)
+      }
+    }
+    camera.drawResults(poses);
+  }
+}
+const shouldStop = false;
+function handleshouldStop() {
+  if (shouldStop) {
+    shouldStop = false;
+    renderPrediction();
+  } else {
+    shouldStop = true;
+  }
+}
+
+let prev_timestamp = 0
+async function renderPrediction(ts) {
+  // console.log("DT IS:", prev_timestamp - ts);
+  if (!shouldStop) {
+    prev_timestamp = ts;
+    await renderResult();
+    rafId = requestAnimationFrame(renderPrediction);
+  }
+};
+
+function initMisc() {
+  posXEl = document.querySelector('#posx pre');
+  posYEl = document.querySelector('#posy pre');
+  posZEl = document.querySelector('#posz pre');
+  calcZEl = document.querySelector('#calcz pre');
+
+  posX3dEl = document.querySelector('#posx3d pre');
+  posY3dEl = document.querySelector('#posy3d pre');
+  posZ3dEl = document.querySelector('#posz3d pre');
+  calcZ3dEl = document.querySelector('#calcz3d pre');
+}
+
+async function app() {
+  // Gui content will change depending on which model is in the query string.
+  // const urlParams = new URLSearchParams(window.location.search);
+  // if (!urlParams.has('model')) {
+  //   alert('Cannot find model in the query string.');
+  //   return;
+  // }
+
+  // await setupDatGui(urlParams);
+
+  initMisc();
+  stats = setupStats();
+
+  camera = await Camera.setupCamera(STATE.camera);
+  console.log("hello", camera);
+
+  await setBackendAndEnvFlags(STATE.flags, STATE.backend);
+
+  detector = await createDetector();
+
+  renderPrediction();
+};
+
+app();
---
+++ headtracking/params.js
@@ -0,0 +1,152 @@
+// import * as posedetection from 'posedetection';
+
+console.log("in params.js", poseDetection);
+
+export const DEFAULT_LINE_WIDTH = 2;
+export const DEFAULT_RADIUS = 4;
+
+export const VIDEO_SIZE = {
+  '640 X 480': {width: 640, height: 480},
+  '640 X 360': {width: 640, height: 360},
+  '360 X 270': {width: 360, height: 270}
+};
+
+
+export const BLAZEPOSE_CONFIG = {
+  maxPoses: 1,
+  type: 'full',
+  scoreThreshold: 0.65,
+  render3D: true,
+  enableSmoothing: true,
+};
+
+export const MOVENET_CONFIG = {
+  maxPoses: 1,
+  type: 'lightning',
+  scoreThreshold: 0.3,
+  customModel: '',
+  enableTracking: false,
+  // render3D: true
+};
+
+
+export const STATE = {
+  camera: {targetFPS: 60, sizeOption: '360 X 270'},
+  backend: 'tfjs-webgl',
+  flags: {},
+  // modelConfig: MOVENET_CONFIG,
+  // model: poseDetection.SupportedModels.MoveNet,
+  modelConfig: BLAZEPOSE_CONFIG,
+  model: poseDetection.SupportedModels.BlazePose,
+};
+
+/**
+ * This map descripes tunable flags and theior corresponding types.
+ *
+ * The flags (keys) in the map satisfy the following two conditions:
+ * - Is tunable. For example, `IS_BROWSER` and `IS_CHROME` is not tunable,
+ * because they are fixed when running the scripts.
+ * - Does not depend on other flags when registering in `ENV.registerFlag()`.
+ * This rule aims to make the list streamlined, and, since there are
+ * dependencies between flags, only modifying an independent flag without
+ * modifying its dependents may cause inconsistency.
+ * (`WEBGL_RENDER_FLOAT32_CAPABLE` is an exception, because only exposing
+ * `WEBGL_FORCE_F16_TEXTURES` may confuse users.)
+ */
+export const TUNABLE_FLAG_VALUE_RANGE_MAP = {
+  WEBGL_VERSION: [1, 2],
+  WASM_HAS_SIMD_SUPPORT: [true, false],
+  WASM_HAS_MULTITHREAD_SUPPORT: [true, false],
+  WEBGL_CPU_FORWARD: [true, false],
+  WEBGL_PACK: [true, false],
+  WEBGL_FORCE_F16_TEXTURES: [true, false],
+  WEBGL_RENDER_FLOAT32_CAPABLE: [true, false],
+  WEBGL_FLUSH_THRESHOLD: [-1, 0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
+  CHECK_COMPUTATION_FOR_ERRORS: [true, false],
+};
+
+export const BACKEND_FLAGS_MAP = {
+  ['tfjs-wasm']: ['WASM_HAS_SIMD_SUPPORT', 'WASM_HAS_MULTITHREAD_SUPPORT'],
+  ['tfjs-webgl']: [
+    'WEBGL_VERSION', 'WEBGL_CPU_FORWARD', 'WEBGL_PACK',
+    'WEBGL_FORCE_F16_TEXTURES', 'WEBGL_RENDER_FLOAT32_CAPABLE',
+    'WEBGL_FLUSH_THRESHOLD'
+  ],
+  ['mediapipe-gpu']: []
+};
+
+export const MODEL_BACKEND_MAP = {
+  [poseDetection.SupportedModels.PoseNet]: ['tfjs-webgl'],
+  [poseDetection.SupportedModels.MoveNet]: ['tfjs-webgl', 'tfjs-wasm'],
+  [poseDetection.SupportedModels.BlazePose]: ['mediapipe-gpu', 'tfjs-webgl']
+}
+
+const TUNABLE_FLAG_NAME_MAP = {
+  PROD: 'production mode',
+  WEBGL_VERSION: 'webgl version',
+  WASM_HAS_SIMD_SUPPORT: 'wasm SIMD',
+  WASM_HAS_MULTITHREAD_SUPPORT: 'wasm multithread',
+  WEBGL_CPU_FORWARD: 'cpu forward',
+  WEBGL_PACK: 'webgl pack',
+  WEBGL_FORCE_F16_TEXTURES: 'enforce float16',
+  WEBGL_RENDER_FLOAT32_CAPABLE: 'enable float32',
+  WEBGL_FLUSH_THRESHOLD: 'GL flush wait time(ms)'
+};
+
+export const params = {
+  VIDEO_SIZE: VIDEO_SIZE,
+  STATE: STATE,
+  DEFAULT_RADIUS: DEFAULT_RADIUS,
+  DEFAULT_LINE_WIDTH, DEFAULT_LINE_WIDTH
+}
+
+export async function setBackendAndEnvFlags(flagConfig, backend) {
+  if (flagConfig == null) {
+    return;
+  } else if (typeof flagConfig !== 'object') {
+    throw new Error(
+        `An object is expected, while a(n) ${typeof flagConfig} is found.`);
+  }
+
+  // Check the validation of flags and values.
+  for (const flag in flagConfig) {
+    // TODO: check whether flag can be set as flagConfig[flag].
+    if (!(flag in TUNABLE_FLAG_VALUE_RANGE_MAP)) {
+      throw new Error(`${flag} is not a tunable or valid environment flag.`);
+    }
+    if (TUNABLE_FLAG_VALUE_RANGE_MAP[flag].indexOf(flagConfig[flag]) === -1) {
+      throw new Error(
+          `${flag} value is expected to be in the range [${
+              TUNABLE_FLAG_VALUE_RANGE_MAP[flag]}], while ${flagConfig[flag]}` +
+          ' is found.');
+    }
+  }
+
+  tf.env().setFlags(flagConfig);
+
+  const [runtime, $backend] = backend.split('-');
+
+  if (runtime === 'tfjs') {
+    await resetBackend($backend);
+  }
+}
+
+/**
+ * Reset the target backend.
+ *
+ * @param backendName The name of the backend to be reset.
+ */
+async function resetBackend(backendName) {
+  const ENGINE = tf.engine();
+  if (!(backendName in ENGINE.registryFactory)) {
+    throw new Error(`${backendName} backend is not registed.`);
+  }
+
+  if (backendName in ENGINE.registry) {
+    const backendFactory = tf.findBackendFactory(backendName);
+    tf.removeBackend(backendName);
+    tf.registerBackend(backendName, backendFactory);
+  }
+
+  await tf.setBackend(backendName);
+}
