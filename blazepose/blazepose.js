4272f4f51ee9708cdbd4546f8d146da6d23880b7
Anton de Winter <adewinter@gmail.com>
2022-02-01 16:46:00 -0800

    .

--- blazepose/blazepose.js
+++
@@ -1,9 +0,0 @@
-const model = poseDetection.SupportedModels.BlazePose;
-const detectorConfig = {
-  runtime: 'mediapipe',
-  solutionPath: 'base/node_modules/@mediapipe/pose'
-};
-detector = await poseDetection.createDetector(model, detectorConfig);
-
-const estimationConfig = {enableSmoothing: true};
-const poses = await detector.estimatePoses(null, estimationConfig);
